5b6ef344de95fef941aed4cb58fdc8701f426e12
Mojmir Horvath <horvathmojmir@gmail.com>
2023-12-29 14:51:27 +0100

    giving it into a function

--- scripts/run.py
+++ scripts/run.py
@@ -1,14 +1,40 @@
 from pdf_processor import PDFProcessor  # Import the PDFProcessor class from pdf_processor.py
-from eval import *
+from eval import query_custom_gpt, convert_json_to_object, save_json_to_file, obj2excel
 
-if __name__ == '__main__':
+def process_pdf_and_convert_to_excel(input_file: str, output_folder: str, json_file_path: str, excel_file_path: str):
+    """
+    Processes a PDF file, performs OCR, queries a custom GPT model,
+    converts the output to a Python object, saves it as a JSON file,
+    and finally converts it to an Excel file.
+
+    :param input_file: Path to the input PDF file.
+    :param output_folder: Folder to save the OCR output.
+    :param json_file_path: Path to save the JSON file.
+    :param excel_file_path: Path to save the Excel file.
+    """
+    # Initialize PDF Processor
     pdf_processor = PDFProcessor()
     pdf_processor.test_cuda()
-    input_file = '../data/3-Batch/3_Doc.pdf'
-    output_folder = '../data/3-Batch/3_Doc_output/'
+
+    # Process PDF and Perform OCR
     ocr_output = pdf_processor.read_in_new_penalty(input_file, output_folder)
+
+    # Query custom GPT model
     json_output = query_custom_gpt(ocr_output)
-    print(json_output)
+
+    # Convert JSON output to Python object
     python_object = convert_json_to_object(json_output)
-    save_json_to_file(python_object, "test.json")
-    obj2excel([python_object, python_object],"./excel/Test.xlsx")
+
+    # Save Python object to JSON file
+    save_json_to_file(python_object, json_file_path)
+
+    # Convert Python object to Excel file
+    obj2excel([python_object, python_object], excel_file_path)
+
+# Example usage
+input_file = '../data/3-Batch/3_Doc.pdf'
+output_folder = '../data/3-Batch/3_Doc_output/'
+json_file_path = './json/test.json'
+excel_file_path = './excel/Test.xlsx'
+
+process_pdf_and_convert_to_excel(input_file, output_folder, json_file_path, excel_file_path)
